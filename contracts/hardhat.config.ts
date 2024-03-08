f08a88b0dda88a3d23e2f8bb04f00b9e5658ad32
kiv1n <aloha@kiv1n.ru>
2024-03-08 18:20:13 +0300

    Add script to deploy profile contract

--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -1,8 +1,17 @@
-import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import * as dotenv from "dotenv";
+import { HardhatUserConfig } from "hardhat/config";
+
+dotenv.config();
 
 const config: HardhatUserConfig = {
   solidity: "0.8.24",
+  networks: {
+    nebulas: {
+      url: process.env.NEBULAS_RPC_URL as string,
+      accounts: [process.env.PRIVATE_KEY as string],
+    },
+  },
 };
 
 export default config;
