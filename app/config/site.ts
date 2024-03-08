0abddc875ca3a449d75537a88dc9519734c69c68
kiv1n <aloha@kiv1n.ru>
2024-03-08 14:42:01 +0300

    Implement landing page

---
+++ app/config/site.ts
@@ -0,0 +1,10 @@
+export type SiteConfig = typeof siteConfig;
+
+export const siteConfig = {
+  emoji: "👻",
+  name: "U2U Friends",
+  description: "Social network in the U2U ecosystem",
+  links: {
+    github: "https://github.com/web3goals/u2u-friends-prototype",
+  },
+};
