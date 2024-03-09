892f9c8b8ff49191e8589ef4ac007caf1afac6e3
kiv1n <aloha@kiv1n.ru>
2024-03-09 17:45:50 +0300

    Display profile metadata on profile page

--- app/components/profile.tsx
+++ app/components/profile.tsx
@@ -1,14 +1,18 @@
 "use client";
 
 import { siteConfig } from "@/config/site";
+import { profileAbi } from "@/contracts/abi/profile";
+import useMetadataLoader from "@/hooks/useMetadataLoader";
 import { emojiAvatarForAddress } from "@/lib/avatars";
 import { addressToShortAddress } from "@/lib/converters";
+import { ProfileMetadata } from "@/types/profile-metadata";
 import Link from "next/link";
 import { isAddressEqual, zeroAddress } from "viem";
-import { useAccount } from "wagmi";
+import { useAccount, useReadContract } from "wagmi";
 import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
 import { Button } from "./ui/button";
 import { Separator } from "./ui/separator";
+import { Skeleton } from "./ui/skeleton";
 
 export function Profile(props: { address: `0x${string}` }) {
   return (
@@ -22,10 +26,28 @@ export function Profile(props: { address: `0x${string}` }) {
 function ProfileHeader(props: { address: `0x${string}` }) {
   const { address } = useAccount();
 
+  /**
+   * Define profile metadata
+   */
+  const { data: metadataUri, isFetched: isMetadataUriFetched } =
+    useReadContract({
+      address: siteConfig.contracts.profile,
+      abi: profileAbi,
+      functionName: "getURI",
+      args: [props.address],
+    });
+  const { data: metadata, isLoaded: isMetadataLoaded } =
+    useMetadataLoader<ProfileMetadata>(metadataUri);
+
   const emojiAvatar = emojiAvatarForAddress(props.address);
+
+  if (!isMetadataUriFetched || !isMetadataLoaded) {
+    return <Skeleton className="h-4" />;
+  }
 
   return (
     <div>
+      {/* Avatar */}
       <Avatar className="size-32">
         <AvatarImage src="" alt="Avatar" />
         <AvatarFallback
@@ -35,16 +57,38 @@ function ProfileHeader(props: { address: `0x${string}` }) {
           {emojiAvatar.emoji}
         </AvatarFallback>
       </Avatar>
-      <div className="flex flex-row gap-2 mt-4">
+      {/* Name */}
+      {metadata?.name && (
+        <h2 className="text-2xl font-bold tracking-tight mt-4">
+          {metadata.name}
+        </h2>
+      )}
+      {/* Bio */}
+      {metadata?.bio && (
+        <p className="text-muted-foreground mt-2">{metadata.bio}</p>
+      )}
+      {/* Links */}
+      <div className="flex flex-row gap-4 mt-4">
         <a
           href={`${siteConfig.contracts.chain.blockExplorers.default.url}address/${props.address}`}
           target="_blank"
           rel="noreferrer"
-          className="font-medium underline underline-offset-4"
+          className="text-sm font-medium underline underline-offset-4"
         >
           {addressToShortAddress(props.address)}
         </a>{" "}
+        {metadata?.website && (
+          <a
+            href={metadata.website}
+            target="_blank"
+            rel="noreferrer"
+            className="text-sm font-medium underline underline-offset-4"
+          >
+            {metadata.website}
+          </a>
+        )}
       </div>
+      {/* Edit button */}
       {isAddressEqual(props.address, address || zeroAddress) && (
         <Link href="/profiles/edit">
           <Button variant="outline" className="mt-4">
--- app/hooks/useMetadataLoader.ts
+++ app/hooks/useMetadataLoader.ts
@@ -7,21 +7,27 @@ import useError from "./useError";
  */
 export default function useMetadataLoader<T>(uri?: string): {
   data: T | undefined;
+  isLoaded: boolean;
 } {
   const { handleError } = useError();
+  const [isLoaded, setIsLoaded] = useState(false);
   const [data, setData] = useState<T | undefined>();
 
   useEffect(() => {
+    setIsLoaded(false);
     setData(undefined);
     if (uri) {
       loadJsonFromIpfs(uri)
         .then((data) => {
           setData(data);
+          setIsLoaded(true);
         })
         .catch((error) => handleError(error, true));
+    } else {
+      setIsLoaded(true);
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [uri]);
 
-  return { data };
+  return { data, isLoaded: isLoaded };
 }
