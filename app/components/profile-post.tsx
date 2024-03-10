dac6ce3be68c829a30781e484af3c56f97c7fa37
kiv1n <aloha@kiv1n.ru>
2024-03-10 14:02:01 +0300

    Add feature to like posts

--- app/components/profile-post.tsx
+++ app/components/profile-post.tsx
@@ -1,15 +1,25 @@
 import { siteConfig } from "@/config/site";
-import { profileAbi } from "@/contracts/abi/profile";
-import useMetadataLoader from "@/hooks/useMetadataLoader";
-import { ProfileMetadata } from "@/types/profile-metadata";
-import { useReadContract } from "wagmi";
-import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
-import { emojiAvatarForAddress } from "@/lib/avatars";
-import { Skeleton } from "./ui/skeleton";
-import { addressToShortAddress } from "@/lib/converters";
 import { postAbi } from "@/contracts/abi/post";
+import { profileAbi } from "@/contracts/abi/profile";
+import useError from "@/hooks/useError";
+import useMetadataLoader from "@/hooks/useMetadataLoader";
+import { emojiAvatarForAddress } from "@/lib/avatars";
+import { addressToShortAddress } from "@/lib/converters";
 import { PostMetadata } from "@/types/post-metadata";
+import { ProfileMetadata } from "@/types/profile-metadata";
+import { Loader2 } from "lucide-react";
+import { useState } from "react";
+import { zeroAddress } from "viem";
+import {
+  useAccount,
+  usePublicClient,
+  useReadContract,
+  useWalletClient,
+} from "wagmi";
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
 import { Button } from "./ui/button";
+import { Skeleton } from "./ui/skeleton";
+import { toast } from "./ui/use-toast";
 
 export function ProfilePost(props: { address: `0x${string}`; post: bigint }) {
   return (
@@ -82,15 +92,82 @@ function PostHeader(props: { address: `0x${string}`; post: bigint }) {
         </p>
         <p className="text-base mt-2">{postMetadata?.text}</p>
         <div className="flex flex-row gap-2 mt-2">
-          <Button variant="outline" size="sm" disabled>
-            Like X
-          </Button>
+          <PostHeaderLikeButton post={props.post} />
           <Button variant="outline" size="sm" disabled>
             Comment
           </Button>
         </div>
       </div>
     </div>
+  );
+}
+
+function PostHeaderLikeButton(props: { post: bigint }) {
+  const { handleError } = useError();
+  const { address } = useAccount();
+  const publicClient = usePublicClient();
+  const { data: walletClient } = useWalletClient();
+  const [isFormSubmitting, setIsFormSubmitting] = useState(false);
+
+  const {
+    data: likers,
+    isFetched: isLikersFetched,
+    refetch: refetchLikers,
+  } = useReadContract({
+    address: siteConfig.contracts.post,
+    abi: postAbi,
+    functionName: "getLikers",
+    args: [props.post],
+  });
+
+  const isLiked = likers?.includes(address || zeroAddress);
+
+  async function onSubmit() {
+    try {
+      setIsFormSubmitting(true);
+      // Check clients
+      if (!publicClient) {
+        throw new Error("Public client is not ready");
+      }
+      if (!walletClient) {
+        throw new Error("Wallet is not connected");
+      }
+      // Send request
+      const { request } = await publicClient.simulateContract({
+        address: siteConfig.contracts.post,
+        abi: postAbi,
+        functionName: "like",
+        args: [props.post],
+        account: address,
+      });
+      const txHash = await walletClient.writeContract(request);
+      const txReceipt = await publicClient.waitForTransactionReceipt({
+        hash: txHash,
+      });
+      // Show success message
+      toast({
+        title: "Liked 👌",
+      });
+      refetchLikers();
+    } catch (error: any) {
+      handleError(error, true);
+    } finally {
+      setIsFormSubmitting(false);
+    }
+  }
+
+  return (
+    <Button
+      variant={isLiked ? "default" : "outline"}
+      size="sm"
+      disabled={!isLikersFetched || isFormSubmitting}
+      onClick={() => onSubmit()}
+    >
+      {(!isLikersFetched || isFormSubmitting) && (
+        <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+      )}
+      Like <span className="ml-1 font-extrabold">{likers?.length || ""}</span>
+    </Button>
   );
 }
 
